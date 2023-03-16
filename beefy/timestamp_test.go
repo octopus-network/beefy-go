2def787e42ba5b71b17fcb205bc4ace0f8246c9a
Boern <boern.x@gmail.com>
2023-03-16 09:57:29 +0800

    add test for timestamp parse

--- beefy/timestamp_test.go
+++ beefy/timestamp_test.go
@@ -18,6 +18,42 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestConvertTimestamp(t *testing.T) {
+	var timestamp uint64 = 1678780392001
+	t.Logf("timestamp uint64: %d ", timestamp)
+	var timestampStr = "2023-03-14 15:53:12.001 +0800 CST"
+	t.Logf("timestampStr: %s", timestampStr)
+
+	timeTemplate1 := "2006-01-02 15:04:05 +0800 CST"
+	t.Logf("timeTemplate1: %s", timeTemplate1)
+	timestampFromstr1, err := time.ParseInLocation(timeTemplate1, timestampStr, time.Local)
+	require.NoError(t, err)
+	t.Logf("timestampFromstr1: %d", timestampFromstr1.UnixMilli())
+	require.Equal(t, timestamp, uint64(timestampFromstr1.UnixMilli()))
+
+	timeTemplate2 := "2006-01-02 15:04:05"
+	t.Logf("timeTemplate2: %s", timeTemplate2)
+	timestampFromstr2, err := time.ParseInLocation(timeTemplate2, timestampStr, time.Local)
+	require.Error(t, err)
+	t.Logf("timestampFromstr2: %d", timestampFromstr2.UnixMilli())
+
+	timestampFromstr3, err := time.Parse(timeTemplate1, timestampStr)
+	require.NoError(t, err)
+	t.Logf("timestampFromstr3: %d", timestampFromstr3.UnixMilli())
+	require.NotEqual(t, timestamp, uint64(timestampFromstr3.UnixMilli()))
+	require.NotEqual(t, uint64(timestampFromstr1.UnixMilli()), uint64(timestampFromstr3.UnixMilli()))
+
+	timestampFromstr4, err := time.Parse(timeTemplate1, timestampStr)
+	require.NoError(t, err)
+	t.Logf("timestampFromstr4: %d", timestampFromstr4.UnixMilli())
+	require.NotEqual(t, timestamp, uint64(timestampFromstr4.UnixMilli()))
+	require.Equal(t, uint64(timestampFromstr3.UnixMilli()), uint64(timestampFromstr4.UnixMilli()))
+
+	// duration, err := time.ParseDuration(timestampStr)
+	// require.NoError(t, err)
+	// t.Logf("duration: %s", duration)
+}
+
 func TestVerifyTimestampProof(t *testing.T) {
 	// composable parachain data
 	key, err := hex.DecodeString("f0c365c3cf59d671eb72da0e7a4113c49f1f0515f462cdcf84e0f1d6045dfcbb")
--- beefy/util_test.go
+++ beefy/util_test.go
@@ -12,8 +12,11 @@ import (
 
 func TestConvSlice(t *testing.T) {
 	value1 := "115 237 235 146 229 61 38 28 11 196 73 222 20 195 104 74 162 139 133 37 211 117 41 203 7 158 175 254 181 101 87 22 116 217 39 159 111 214 185 199 85 80 62 166 217 178 36 218 53 83 37 138 100 7 169 18 128 57 23 178 111 191 27 245 1"
+	t.Logf("Raw value: %#v", value1)
 	convValue1 := conv.SplitStrToSlice[byte](value1, " ")
 	t.Logf("Split str to uint slice: %v", convValue1)
+	t.Logf("Split str to uint slice: %#v", convValue1)
+	t.Logf("Split str to uint slice: %+v", convValue1)
 	value2 := "190, 171, 181, 52, 208, 35, 61, 63, 243, 167, 41, 72, 146, 79, 19, 208, 223,177, 46, 195, 87, 235, 1, 167, 227, 185, 178, 150, 73, 165, 92, 75"
 	t.Logf("Raw value: %v", value2)
 	replacedStr := strings.ReplaceAll(value2, ", ", ",")
